65a8f5be556fe111186e30bef2592311dec25ff0
vlad solimtsyk <vlad.solimtsyk@gmail.com>
2018-05-17 22:45:43 +0300

    Add project files.

---
+++ DAL/Models/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+        public virtual ICollection<Message> Messages { get; set; }
+    }
+}
